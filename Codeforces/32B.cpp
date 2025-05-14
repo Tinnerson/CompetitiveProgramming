3ef225dc94f60362d644dc43420083b708a564ce
류인호 <128089943+Tinnerson@users.noreply.github.com>
2025-05-14 16:16:06 -0600

    Update 32B.cpp

--- Codeforces/32B.cpp
+++ Codeforces/32B.cpp
@@ -1,27 +1,27 @@
-  #include <iostream>
-     
-    using namespace std;
-     
-    int main()
-    {
-        string b;
-        cin>>b;
-        
-        string c = "";
-        
-        for(int i=0; i<b.size(); i++){
-            if(b[i] == '.'){
-                c += '0';
-            }
-            else if(b[i] =='-' && b[i+1] == '.'){
-                c +='1';
-                i++;
-            }
-            else if(b[i] == '-' && b[i+1] == '-'){
-                c+="2";
-                i++;
-            }
-        }
-        cout<<c<<"\n";
-        return 0;
-    }
+#include <iostream>
+   
+  using namespace std;
+   
+  int main()
+  {
+      string b;
+      cin>>b;
+      
+      string c = "";
+      
+      for(int i=0; i<b.size(); i++){
+          if(b[i] == '.'){
+              c += '0';
+          }
+          else if(b[i] =='-' && b[i+1] == '.'){
+              c +='1';
+              i++;
+          }
+          else if(b[i] == '-' && b[i+1] == '-'){
+              c+="2";
+              i++;
+          }
+      }
+      cout<<c<<"\n";
+      return 0;
+  }
