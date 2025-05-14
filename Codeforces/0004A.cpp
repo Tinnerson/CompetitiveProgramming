75ff8e8e9ae60108428625919aef136c46ae56b6
류인호 <128089943+Tinnerson@users.noreply.github.com>
2025-05-14 17:45:29 -0600

    Create 0004A.cpp

---
+++ Codeforces/0004A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ar array
+ 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin>>n;
+
+	if(n%2==0 && n != 2){
+		cout<<"YES"<<"\n";
+	}
+	else{
+		cout<<"NO"<<"\n";
+	}
+}
