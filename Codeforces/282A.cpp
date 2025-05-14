b53e22f972dc765fe9b77851b6a9c58281ebe327
류인호 <128089943+Tinnerson@users.noreply.github.com>
2025-05-14 16:14:22 -0600

    Create 282A.cpp

---
+++ Codeforces/282A.cpp
@@ -0,0 +1,26 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+     
+    int main()
+    {
+    	int n;
+    	string b;
+    	cin >> n;
+     
+    	int a = 0;
+     
+     
+    	for (int i = 0; i < n; i++) {
+    		cin >> b;
+    		if (b == "X++" || b == "++X") {
+    			a += 1;
+    		}
+    		else if (b == "X--" || b == "--X") {
+    			a -= 1;
+    		}
+    	}
+    	cout << a << "\n";
+    	return 0;
+    }
