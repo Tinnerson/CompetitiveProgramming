1a7fc2e5c3351688c2ed8496d5f9b9958047cc27
류인호 <128089943+Tinnerson@users.noreply.github.com>
2025-05-14 22:22:26 -0600

    Create 0084A.cpp

---
+++ Codeforces/0084A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	double n;
+	cin>>n;
+
+	cout<<fixed<<setprecision(0)<<n+n/2<<"\n";
+}
