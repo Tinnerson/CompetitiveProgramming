97f756eee270e09f3237c67f238295d4934c00db
류인호 <128089943+Tinnerson@users.noreply.github.com>
2025-05-14 22:43:04 -0600

    Create 1549A.cpp

---
+++ Codeforces/1549A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		cout<<(n-1)/2<<" "<<n-1<<"\n";
+	}
+
+}
